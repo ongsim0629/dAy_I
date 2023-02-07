852845f9ea4c0512d66927bdc0f97f3e3ae9a3b4
ImxYJL <111052302+ImxYJL@users.noreply.github.com>
2023-02-07 21:34:16 +0900

    feat: Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import LoginPage from './Login/LoginPage';
 //import './index.css';
 import RegisterPage from './Register/RegisterPage'; 
-//import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RegisterPage />
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
-
-
-//reportWebVitals();
