6caaa5fafc04f758e369460fa130b0c338393980
Konahyeon <koo@inha.edu>
2023-02-07 19:41:11 +0900

    feat: 회원가입 중복 확인 버튼 및 라우터 기능

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import './App.css';
+import LoginPage from "./Login/LoginPage";
+import RegisterPage from "./Register/RegisterPage";
+import IndexPage from "./Index/IndexPage"
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+      <div className="App">
+        <Routes>
+            <Route exact path="/" element={<IndexPage/>}/> 
+            <Route path="/members/register" element={<RegisterPage/>}/>  
+            <Route path ="/members/login" element={<LoginPage/>}/>
+        </Routes>
+
+      </div>
+
+    
+    
+  );
+};
