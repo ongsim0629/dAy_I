a5f58872c4c8083c06b7a5c27c7e50dcd33574be
ImxYJL <allensain14@gmail.com>
2023-02-08 17:34:10 +0900

    feat: 라우터에 EditPage 경로 추가

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./Login/LoginPage";
 import RegisterPage from "./Register/RegisterPage";
 import IndexPage from "./Index/IndexPage"
+import EditPage from "./Edit/EditPage";
 
 function App(){
   return (
@@ -13,6 +14,7 @@ function App(){
             <Route exact path="/" element={<IndexPage/>}/> 
             <Route path="/members/register" element={<RegisterPage/>}/>  
             <Route path ="/members/login" element={<LoginPage/>}/>
+            <Route path="/members/test/edit" element={<EditPage/>}/>
         </Routes>
 
       </div> 
