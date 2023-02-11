4137cd2fb0412cec8a52ca56a39eadb34ebca570
ImxYJL <allensain14@gmail.com>
2023-02-11 19:49:50 +0900

    feat: Update WritePage.jsx

---
+++ front-end/src/Edit/CustomModal.js
@@ -0,0 +1,92 @@
+import {React, useEffect} from "react";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Background = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255,255,255,0.15);
+    backdrop-filter: blur(5px);
+    animation: modal-bg-show 1s;
+    @keyframes modal-bg-show {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+`;
+
+const ModalBlock = styled.div`
+    position: absolute;
+    top: 6.5rem;
+    border-radius: 10px;
+    padding: 1.5rem;
+    background-color: black;
+    width: 60rem;
+    @media (max-width: 1120px) {
+        width: 50rem;
+    }
+    @media (max-width: 50rem) {
+        width: 80%;
+    }
+    min-height: 35rem;
+    animation: modal-show 1s;
+    @keyframes modal-show {
+        from {
+            opacity: 0;
+            margin-top: -50px;
+        }
+        to {
+            opacity: 1;
+            margin-top: 0;
+        }
+    }
+`;
+
+const Close = styled.img.attrs({
+    src: window.Close,
+})`
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    cursor: pointer;
+`;
+
+const Contents = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const CustomModal = ({ _handleModal, children, ...rest }) => {
+  return (
+      <Container>
+          <Background onClick={_handleModal} />
+          <ModalBlock {...rest}>
+              <Close onClick={_handleModal} />
+              <Contents>
+                  {children}
+              </Contents>
+          </ModalBlock>
+      </Container>
+  );
+};
+
+//export default CustomModal;
