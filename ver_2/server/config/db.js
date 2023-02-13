adc6474f5068cf8d4b0667f4cd200ac35cddaff0
s1_1v <s1_1v@naver.com>
2023-02-13 10:40:50 +0900

    프록시, 라우터 설정 및 경로 정리

---
+++ ver_2/server/config/db.js
@@ -0,0 +1,8 @@
+var mysql = require('mysql');
+const db = mysql.createPool({
+    host : 'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'gyeongin' //db명
+})
+module.exports = db;
