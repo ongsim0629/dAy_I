26c540f84cc775e8250d5a5c56c64eeec6231c92
sehyun <goozip2@kyonggi.ac.kr>
2023-02-15 17:49:15 +0900

    Diary.js sql문까지 성공

--- ver_2/client/src/Home/HomePage.jsx
+++ ver_2/client/src/Home/HomePage.jsx
@@ -1,77 +1,85 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import DatePicker from "react-datepicker";
-import "./DatePicker.css"
+import "./DatePicker.css";
 import "react-datepicker/dist/react-datepicker.css";
 //import ko from 'date-fns/locale/ko';
 
 const Header = styled.header`
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100px;
-    display: flex;
-    flex-direction: row;
-    background-color: #E5E0FF;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  background-color: #e5e0ff;
 `;
 
 const Layout = styled.div`
-    padding-top:25px;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  padding-top: 25px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const CustomButton = styled.button`
-    color: #8F8F8F;
-    background: #F5F5F5;
-    font-weight: bold;
-    border: none;
-    margin-top: 32px;
-    border-radius: 4px;
-    padding: 7px 10px;
+  color: #8f8f8f;
+  background: #f5f5f5;
+  font-weight: bold;
+  border: none;
+  margin-top: 32px;
+  border-radius: 4px;
+  padding: 7px 10px;
 
-    &:hover {
-        cursor: pointer;
-    }
-    &:focus{
-        box-shadow: 0 0 0 1px gray;
-    }
+  &:hover {
+    cursor: pointer;
+  }
+  &:focus {
+    box-shadow: 0 0 0 1px gray;
+  }
 `;
 
-        // 응답(항상 실행)
+// 응답(항상 실행)
 
 function HomePage() {
   const [id, setId] = useState("");
   const [startDate, setStartDate] = useState(new window.Date());
   const navigate = useNavigate();
 
-  axios.get('/members/edit')
-        .then(function () {
-            const token = localStorage.getItem("token");
-            var base64Url = token.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
-            var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); 
-            var result = JSON.parse(decodeURIComponent(atob(base64).split('').map(function(c) {
-                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-            }).join('')));
-            let login_id = result.user_id;
-            console.log(login_id);
-            setId(login_id);
-        })
-        // 응답(실패)
-        .catch(function (error) {
-            console.log(error);
-        })
+  axios
+    .get("/members/edit")
+    .then(function () {
+      const token = localStorage.getItem("token");
+      var base64Url = token.split(".")[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
+      var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      var result = JSON.parse(
+        decodeURIComponent(
+          atob(base64)
+            .split("")
+            .map(function (c) {
+              return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+        )
+      );
+      let login_id = result.user_id;
+      console.log(login_id);
+      setId(login_id);
+    })
+    // 응답(실패)
+    .catch(function (error) {
+      console.log(error);
+    });
 
   // 로그아웃
   const onLogoutButtonHandler = () => {
-    let token = localStorage.getItem('token')
-    localStorage.clear()
-  }
+    let token = localStorage.getItem("token");
+    localStorage.clear();
+  };
 
   //문자열 변환
   const dateToString = () => {
@@ -83,22 +91,22 @@ function HomePage() {
     }`;
   };
 
-  const onDatePickHandler = async(date)=>{
+  const onDatePickHandler = async (date) => {
     setStartDate(date);
 
     //diary 있는 경우로 조건 달아줘야 함
     //diary로 이동 시 URL로 date, user_id 전달해야 함
-    await axios.post('/diaries/test/id', {
-      id: id,
-      date: dateToString(startDate)
-    })
-    .then((res) => {
-      console.log("서버로 date와 id가 전달되었습니다.")
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-
+    await axios
+      .post("/diaries/test/id", {
+        id: id,
+        date: dateToString(startDate),
+      })
+      .then((res) => {
+        console.log("서버로 date와 id가 전달되었습니다.");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
     // await axios({
     //   method: 'get',
@@ -109,41 +117,46 @@ function HomePage() {
     //   }
     // })
 
-
-    //작성한 내용이 있으면 읽기 페이지로, 없으면 쓰기 페이지로 가야 하지만 
+    //작성한 내용이 있으면 읽기 페이지로, 없으면 쓰기 페이지로 가야 하지만
     //일단 선택한 날짜의 읽기 페이지로 가도록 함
-    navigate("/members/test/write", {state: {selectedDate: date}});
-    
-}
+    navigate("/members/test/write", { state: { selectedDate: date } });
+  };
   return (
     <>
       <Header>
         <Link to="/">
-          <CustomButton onClick={onLogoutButtonHandler} style={{marginLeft:'1320px'}}>로그아웃</CustomButton>
+          <CustomButton
+            onClick={onLogoutButtonHandler}
+            style={{ marginLeft: "1320px" }}
+          >
+            로그아웃
+          </CustomButton>
         </Link>
         <Link to="/members/test/mypage">
-          <CustomButton type='button' style={{marginLeft:'20px'}}>마이페이지</CustomButton>
+          <CustomButton type="button" style={{ marginLeft: "20px" }}>
+            마이페이지
+          </CustomButton>
         </Link>
       </Header>
-      <Layout> 
-            {/* <h4>{id}님 환영합니다</h4>
+      <Layout>
+        {/* <h4>{id}님 환영합니다</h4>
             <Link to="/diaries/id/date">
               <h5>10일</h5>
             </Link> */}
-            <h2 style={{color:'#AEAEAE', fontFamily:'AbeeZee'}}>당신의 소중한 하루를 기록해보세요.</h2><br/>
-            <DatePicker
-                onChange={(date)=>onDatePickHandler(date)}
-                selected={startDate}
-                // {/* locale={ko} */}
-               //  {/* highlightDates={[]} */}
-                className="datePicker"
-                inline
-                >
-      </DatePicker>
-            
-      </Layout> 
+        <h2 style={{ color: "#AEAEAE", fontFamily: "AbeeZee" }}>
+          당신의 소중한 하루를 기록해보세요.
+        </h2>
+        <br />
+        <DatePicker
+          onChange={(date) => onDatePickHandler(date)}
+          selected={startDate}
+          // {/* locale={ko} */}
+          //  {/* highlightDates={[]} */}
+          className="datePicker"
+          inline
+        ></DatePicker>
+      </Layout>
     </>
-    
   );
 }
 
--- ver_2/server/server.js
+++ ver_2/server/server.js
@@ -14,7 +14,7 @@ app.use("/members/new", registerRouter);
 app.use("/members/login", loginRouter);
 app.use("/members/edit", editRouter);
 app.use("/members/test/write", writeRouter); //test 끼워넣기는 front팀과 일치시키기 위함
-app.use("/members/diary/:id/:date", diaryRouter); //GET이기에 콜론 작성 (token은 보류)
-
+app.use("/diaries/test/id", diaryRouter); //GET이기에 콜론 작성 (token은 보류)
+///members/diary/:id/:date
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
