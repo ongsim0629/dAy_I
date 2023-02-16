ff7ae70c4339df9797b3ef04e9e427f3c949f788
JunHyeok_Yoo <114297987+JunHyeokYoo@users.noreply.github.com>
2023-02-16 09:40:02 +0900

    다이어리 db 저장

--- ver_2/server/routes/write.js
+++ ver_2/server/routes/write.js
@@ -8,6 +8,8 @@ router.use(express.urlencoded({ extended: false }));
 
 var request = require('request');
 
+
+
 //date(String), title(), content, (token) 전달받음
 router.post("/", (req, res) => {
   console.log("/members/test/write 호출됨");
@@ -30,6 +32,10 @@ router.post("/", (req, res) => {
   console.log("title", title);
   console.log("content", content);
 
+  db.query("INSERT INTO diary (DIARY_WRITER_ID, DIARY_WRITE_DATE, DIARY_TITLE, DIARY_CONTENT) VALUES (?, ?, ?, ?);",['qwe',date,title,content], function (error, results, fields) {
+    if (error) throw error;
+  });
+
   // FLASK로 일기 내용 보내기
   const ModelResult  = (callback)=>{
     const options = {
@@ -47,47 +53,58 @@ router.post("/", (req, res) => {
   }
 
   ModelResult((err, {result}={})=>{
-    if(err){
-        console.log("error!!!!");
-        res.send({
-            message: "fail",
-            status: "fail"
-        });
-    }
+    // if(err){
+    //     console.log("error!!!!");
+    //     res.send({
+    //         message: "fail",
+    //         status: "fail"
+    //     });
+    // }
+    let json = JSON.parse(result); // 플라스크에서 받은 json 값
 
-    let json = JSON.parse(result);
-
-    console.log(json.key_list);
-
-    //플레이리스트 id추출
-    let emotion = json.emotion;
+    //플레이리스트 id추출 부분==================
+    let emotion = json.emotion; //받은 json에서 감정 한개 받음
     db.query("SELECT PLAYLIST_ID, PLAYLIST_TITLE FROM playlist WHERE PLAYLIST_EMOTION = ? ORDER BY RAND() LIMIT 1;",emotion, function (error, results, fields) {
       if (error) throw error;
-    // console.log('records: ', results[0].PLAYLIST_ID);
+      console.log('감정: ', emotion);
+      console.log('플리id: ', results[0].PLAYLIST_ID); // results[0].PLAYLIST_ID 가 랜덤으로 뽑은 플레이리스트 ID
+      db.query("UPDATE diary SET DIARY_EMOTION = ?,DIARY_PLAYLIST = ? WHERE DIARY_WRITER_ID = ? and DIARY_WRITE_DATE = ?;",[emotion,results[0].PLAYLIST_ID,'qwe',date], function (error, results, fields) {
+        if (error) throw error;
+      });
+      
     });
 
-    //키워드에 대한 카테고리 추출
+    //키워드에 대한 카테고리 추출===============
     let key_arr = [];
-    let key_arr2 = [];
-    key_arr = json.key_list;
-    var len = 10 - key_arr.length
-    for(var i = 0; i < len; i++){
+    let key_arr_sum = []; //sql 쿼리문 깔끔하게 담기 위해 합칠 임시 배열
+    key_arr = json.key_list; // flask 에서 받은 키워드를 리스트로 받음
+    var len = 10 - key_arr.length // 키워드가 10개 미만 뽑혔을때를 대비한 길이 우선 지정
+
+    for(var i = 0; i < len; i++){ //키워드 10개 미만이면 0으로 10개에 맞게 채워놓음
       key_arr.push('0');
-    } 
-    // console.log(key_arr[1]);
-    let cate = "";
-    key_arr2 = key_arr.concat(key_arr)
-    db.query("SELECT CATEGORY FROM keyword where keyword in (?,?,?,?,?,?,?,?,?,?) ORDER BY FIELD(keyword,?,?,?,?,?,?,?,?,?,?) limit 1;",key_arr2,function (error, results) {
+    }
+
+    key_arr_sum = key_arr.concat(key_arr)
+    db.query("SELECT * FROM keyword where keyword in (?,?,?,?,?,?,?,?,?,?) ORDER BY FIELD(keyword,?,?,?,?,?,?,?,?,?,?) limit 1;",key_arr_sum,function (error, results) {
       if (error) throw error;
       if (results.length > 0) {
-        cate = results[0].CATEGORY
-        console.log('cate:'+ cate)
+        const catego = results[0].CATEGORY
+        console.log('카테고리:'+ catego)
+        db.query("UPDATE diary SET DIARY_KEYWORD = ? WHERE DIARY_WRITER_ID = ? and DIARY_WRITE_DATE = ?;",[results[0].KEYWORD,'qwe',date], function (error, results, fields) {
+          if (error) throw error;
+        });
+        db.query("SELECT SITE_URL FROM categorysite where SITE_CATEGORY = ?;",[results[0].CATEGORY], function (error, results, fields) {
+          if (error) throw error;
+          db.query("UPDATE diary SET DIARY_CATEGORY_SITE = ? WHERE DIARY_WRITER_ID = ? and DIARY_WRITE_DATE = ?;",[results[0].SITE_URL,'qwe',date], function (error, results, fields) {
+            if (error) throw error;
+          });
+        });
+
       } else {
         console.log('No records found.');
       }
     });
-    }
-  )
+  });
 
   db.query(query, (err, data) => {
     if (!err) {
@@ -96,6 +113,25 @@ router.post("/", (req, res) => {
       console.log("오류");
     }
   });
+
+  function getMyVariable() {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT PLAYLIST_ID, PLAYLIST_TITLE FROM playlist WHERE PLAYLIST_EMOTION = "슬픔" ORDER BY RAND() LIMIT 1;';
+      db.query(sql, function (error, results, fields) {
+        if (error) reject(error);
+        const myVariable = results[0].PLAYLIST_ID;
+        resolve(myVariable);
+      });
+    });
+  }
+  
+  // Call the function and output the variable log outside
+  getMyVariable().then((myVariable) => {
+    console.log(myVariable); // Output: the value of the first row of the "my_column" column in the "mytable" table
+  }).catch((error) => {
+    console.log(error);
+  });
+
 });
 
 module.exports = router;
