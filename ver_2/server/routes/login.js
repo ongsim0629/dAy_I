5a2165e5546ec7b5c14238e5d9b1b7fe29405fbf
s1_1v <s1_1v@naver.com>
2023-02-16 15:29:44 +0900

    fix: login api 수정

--- ver_2/server/routes/login.js
+++ ver_2/server/routes/login.js
@@ -1,11 +1,8 @@
 var express = require("express");
 var router = express.Router();
-
-//var jwt = require("../node_modules/jsonwebtoken"); //추가
-
 var jwt = require("jsonwebtoken");
-
 const db = require("../../server/config/db.js");
+
 const crypto = require("crypto");
 
 router.use(express.json());
@@ -23,46 +20,92 @@ router.post("/", (req, res) => {
     "select salt, user_password from user where user_id='" + id + "';";
   console.log(">>>입력한 아이디: ", id, ", 비밀번호: ", password);
 
-  db.query(query, (err, data) => {
-    if (!err) {
-      // 결과값이 1보다 작다면(동일한 id 가 없다면)
-      if (data.length == 0) {
-        console.log(">>>아이디 없어유");
-        res.send({ msg: "입력하신 id가 일치하지 않습니다." });
-        return;
-      } else {
-        // 동일한 id 가 있으면 비밀번호 일치 확인
-        console.log(">>>아이디 있어유");
-        var salt = data[0].salt;
-        var password_db = data[0].user_password;
-        console.log(data[0].salt);
-        console.log(data[0].user_password);
-        const hash = crypto
-          .createHash("sha512")
-          .update(password + salt)
-          .digest("hex");
-        console.log(hash);
-        if (password_db == hash) {
-          console.log("로그인 성공");
 
-          var token = jwt.sign(
-            {
-              user_id: id, //Private Claim 자리 (키: 데이터)
-            },
-            "secretKey", //Signature (비밀키가 들어갈 자리)
-            {
-              subject: "gyeongInLine jwtToken", //Public Claim 자리 (부가정보 자리)
-              expiresIn: "60m",
-              issuer: "gyeongInLine",
-            }
-          );
-
-          console.log("토큰 생성", token);
-
-          res.send({ userId: id, token: token });
-        }
-      }
+  db.getConnection((err, conn) => {
+    //db 연결 실패 시,
+    if (err) {
+      console.log("Mysql 연결 실패");
+      conn.release(); //커넥션 풀에 커넥션 반환 -> 연결 해제
+      res.writeHead("404", { "content-Type": "text/html; charset=utf8" });
+      res.write("<h2>DB 서버 연결 실패</h2>");
+      res.end();
+      return;
     }
+    // db 연결 성공 시
+    console.log("데이터베이스 conn");
+
+    const exec = conn.query(
+      "select salt, user_password from user where user_id='" + id + "';",
+      (err, data) => {
+        console.log("실행된 SQL: " + exec.sql);
+        // sql 오류 시
+        if (!err) {
+          // 결과값이 1보다 작다면(동일한 id 가 없다면)
+          if (data.length == 0) {
+            console.log(">>>아이디 없어유");
+            res.send({ msg: "입력하신 id가 일치하지 않습니다." });
+            return;
+          } else {
+            // 동일한 id 가 있으면 비밀번호 일치 확인
+            console.log(">>>아이디 있어유");
+            var salt = data[0].salt;
+            var password_db = data[0].user_password;
+            console.log(data[0].salt);
+            console.log(data[0].user_password);
+            const hash = crypto
+              .createHash("sha512")
+              .update(password + salt)
+              .digest("hex");
+            console.log(hash);
+            if (password_db == hash) {
+              console.log("로그인 성공");
+    
+              var token = jwt.sign(
+                {
+                  user_id: id, //Private Claim 자리 (키: 데이터)
+                },
+                "secretKey", //Signature (비밀키가 들어갈 자리)
+                {
+                  subject: "gyeongInLine jwtToken", //Public Claim 자리 (부가정보 자리)
+                  expiresIn: "60m",
+                  issuer: "gyeongInLine",
+                }
+              );
+    
+              console.log("토큰 생성", token);
+
+              conn.query(
+                "select diary_write_date from diary where diary_writer_id ='" + id + "';",
+                (err, result) => {
+                  conn.release();
+                  // sql 오류 시
+                  if (err) {
+                    console.log("SQL 실행 시, 오류 발생");
+                    console.dir(err);
+                    res.writeHead("200", { "content-Type": "text/html; charset=utf8" });
+                    res.write("<h2>SQL 실행 실패;</h2>");
+                    res.status(404).send("오류");
+                    res.end();
+                    return;
+                  } else {
+                    // sql 성공 시
+                    // 하루에 일기 1개 초과 시
+                    var dataList = [];
+                    for (var data of result){
+                      dataList.push(new Date(data.diary_write_date));
+                    };
+                    console.log(dataList);
+        
+                    res.send({ userId: id, token: token, list:dataList });
+                  }
+                }
+              );
+  
+            }
+          }
+        }
+      });
+
   });
 });
 
