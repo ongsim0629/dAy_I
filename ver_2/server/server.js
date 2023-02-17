6ab758a395a397d5d3296bd6d43f2bb3e85ca55f
Konahyeon <koo@inha.edu>
2023-02-18 02:32:37 +0900

    fix: diaryRouter 변경

--- ver_2/server/server.js
+++ ver_2/server/server.js
@@ -16,7 +16,7 @@ app.use("/members/new", registerRouter);
 app.use("/members/login", loginRouter);
 app.use("/members/edit", editRouter);
 app.use("/members/test/write", writeRouter); //test 끼워넣기는 front팀과 일치시키기 위함
-app.use("/diaries/test/id", diaryRouter); //GET이기에 콜론 작성 (token은 보류)
+app.use("/diaries", diaryRouter); //GET이기에 콜론 작성 (token은 보류)
 ///members/diary/:id/:date
 app.use("/members/delete", diaryDeleteRouter);
 app.use("/members/mypage", mypageRouter);
