fd80a87ece9939947322ea7cc7af624708c90259
sehyun <goozip2@kyonggi.ac.kr>
2023-02-20 16:03:51 +0900

    token 재생성 없이 이전버튼 오류 고치기 완료

--- ver_2/server/routes/home.js
+++ ver_2/server/routes/home.js
@@ -9,73 +9,57 @@ router.use(express.urlencoded({ extended: false }));
 process.setMaxListeners(0);
 
 router.post("/", (req, res) => {
-    //변수 설정
-    console.log("/members/home 호출됨");
-    const token = req.body.token;
-    console.log("토큰확인입니다:"+token);
+  //변수 설정
+  console.log("/members/home 호출됨");
+  const token = req.body.token;
+  //const date = req.body.date;
+  //const yearMonth = date.substring(0, 8) + "__"; //연월   2023-02-13
+  console.log("토큰확인입니다:" + token);
 
-    try {
-        var check = jwt.verify(token, "secretKey");
-        if (check) {
-          console.log("token 검증", check.user_id);
+  try {
+    var check = jwt.verify(token, "secretKey");
+    if (check) {
+      console.log("token 검증", check.user_id);
+    }
+  } catch {
+    console.log("token 검증 오류");
+  }
+
+  const id = check.user_id;
+
+  db.getConnection((err, conn) => {
+    //db 연결 실패 시,
+    if (err) {
+      console.log("Mysql 연결 실패");
+      conn.release(); //커넥션 풀에 커넥션 반환 -> 연결 해제
+      res.writeHead("404", { "content-Type": "text/html; charset=utf8" });
+      res.write("<h2>DB 서버 연결 실패</h2>");
+      res.end();
+      return;
+    }
+    // db 연결 성공 시
+    console.log("데이터베이스 conn");
+
+    const exec = conn.query(
+      "select diary_write_date from diary where diary_writer_id ='" + id + "';",
+      (err, result) => {
+        conn.release();
+        // sql 오류 시
+        if (err) {
+          console.log("SQL 실행 시, 오류 발생");
+          console.dir(err);
+          res.writeHead("200", { "content-Type": "text/html; charset=utf8" });
+          res.write("<h2>SQL 실행 실패;</h2>");
+          res.status(404).send("오류");
+          res.end();
+          return;
+        } else {
+          // sql 성공 시
+          res.send({ list: result, token: token });
         }
-      } catch {
-        console.log("token 검증 오류");
       }
-    
-      const id = check.user_id;
-
-      var token_2 = jwt.sign(
-        {
-          user_id: id, //Private Claim 자리 (키: 데이터)
-        },
-        "secretKey", //Signature (비밀키가 들어갈 자리)
-        {
-          subject: "gyeongInLine jwtToken", //Public Claim 자리 (부가정보 자리)
-          expiresIn: "60m",
-          issuer: "gyeongInLine",
-        }
-      );
-
-      console.log("토큰 생성", token);
-    
-  
-    db.getConnection((err, conn) => {
-      //db 연결 실패 시,
-      if (err) {
-        console.log("Mysql 연결 실패");
-        conn.release(); //커넥션 풀에 커넥션 반환 -> 연결 해제
-        res.writeHead("404", { "content-Type": "text/html; charset=utf8" });
-        res.write("<h2>DB 서버 연결 실패</h2>");
-        res.end();
-        return;
-      }
-      // db 연결 성공 시
-      console.log("데이터베이스 conn");
-  
-      const exec = conn.query(
-                  "select diary_write_date from diary where diary_writer_id ='" + id + "';",
-                  (err, result) => {
-                    conn.release();
-                    // sql 오류 시
-                    if (err) {
-                      console.log("SQL 실행 시, 오류 발생");
-                      console.dir(err);
-                      res.writeHead("200", { "content-Type": "text/html; charset=utf8" });
-                      res.write("<h2>SQL 실행 실패;</h2>");
-                      res.status(404).send("오류");
-                      res.end();
-                      return;
-                    } else {
-                      // sql 성공 시
-                      res.send({  list:result , token: token_2});
-                    }
-                  }
-                );
-    
-              })
-  
+    );
   });
-  
-  module.exports = router;
-  
+});
+
+module.exports = router;
