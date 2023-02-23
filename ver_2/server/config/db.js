4a5e30ff4c52626b1f7ed52d97fc5eeeeb473423
euna0104 <koeuna0104@naver.com>
2023-02-23 16:31:12 +0900

    파비콘, 회원등록시 비밀번호 유효성, 회원정보수정 오타 정정

--- ver_2/server/config/db.js
+++ ver_2/server/config/db.js
@@ -2,8 +2,8 @@ var mysql = require("mysql");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "1234",
-  database: "gyeongin", //db명
+  password: "euna",
+  database: "my_db", //db명
 
 });
 module.exports = db;
