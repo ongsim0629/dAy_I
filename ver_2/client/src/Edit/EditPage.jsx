878758f5c18adac90c73fcac62b9e6c4d7637564
Konahyeon <koo@inha.edu>
2023-02-24 15:23:40 +0900

    feat: 회원정보수정 로고 페이지 이동

--- ver_2/client/src/Edit/EditPage.jsx
+++ ver_2/client/src/Edit/EditPage.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 import Logo from "../Image/Logo.png";
 
 const Header = styled.header`
-  position: fixed;
   top: 0;
   width: 100%;
   height: 100px;
@@ -219,21 +218,36 @@ function EditPage(props) {
         navigate("/members/login");
       });
   };
+  
+  const onBackHandler = async (event) => {
+    event.preventDefault();
+
+    console.log("이전 버튼 클릭!");
+
+    const result = await axios
+      .post("/members/tohome", {
+        //서버로 id, password 전달
+        token: sessionStorage.getItem("token"),
+        date : dateToString(new window.Date())
+      })
+      .then((res) => {
+        console.log(res);
+        navigate("/members/home", { state: { dataList: res.data.dataList, summaryList: res.data.summaryList }  });
+      });
+  };
 
   return (
     <div>
       <Header>
-        <img src={Logo} style={{ margin: '30px', marginTop: '30px', height: '40px' }} />
+        <img onClick={onBackHandler} src={Logo} style={{ margin: '30px', marginTop: '30px', height: '40px' }} />
       </Header>
-      <div>
+      <div style={{marginTop: '4%'}}>
         <div
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             width: "100%",
-            height: "100vh",
-            position: "fixed",
           }}
         >
           <div>
